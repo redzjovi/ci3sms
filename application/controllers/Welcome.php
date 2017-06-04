7c7d7d40c9d181a8054d567fcc54c9cf253eb280
Jovi <redzjovi@gmail.com>
2017-06-04 10:07:46 +0700

    Update library.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        redirect('backend/admin');
+		// $this->load->view('welcome_message');
 	}
 }
