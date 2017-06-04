4a16fcd4df8438c684f1b19031ee92637a1e917a
Jovi <redzjovi@gmail.com>
2017-06-04 10:17:41 +0700

    Update top menu.

--- application/views/backend/_layouts/top_menu.php
+++ application/views/backend/_layouts/top_menu.php
@@ -10,12 +10,12 @@
     </div>
     <!-- /.navbar-header -->
 
-    <ul class="nav navbar-top-links navbar-right pull-right">
+    <ul class="nav navbar-top-links navbar-right">
         <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                 <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
             </a>
-            <ul class="dropdown-menu dropdown-user">
+            <ul class="dropdown-menu dropdown-messages">
                 <li>
                     <a href="<?= site_url('backend/admin/logout'); ?>"><i class="fa fa-sign-out fa-fw"></i> <?= ucfirst(lang('logout')); ?></a>
                 </li>
